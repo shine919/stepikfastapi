c80d5e5361f0ac72ac0404a3924e1bb4e70f6759
shine919 <overuber@hotmail.com>
2025-07-11 16:17:22 +0400

    load config to config

--- app/config.py
+++ app/config.py
@@ -13,6 +13,7 @@ class Config:
     db: DataBaseConfig
     secret_key: str
     debug: bool
+    salt: str
 
 
 def load_config(path: str = None) -> Config:
@@ -23,4 +24,13 @@ def load_config(path: str = None) -> Config:
         db=DataBaseConfig(database_url=env.str("DATABASE_URL")),
         secret_key=env.str("SECRET_KEY"),
         debug=env.bool("DEBUG", default=False),
+        salt=env.str("SALT"),
     )
+
+
+config = load_config()
+
+fake_db = [
+    {"user_id": "b6b8770e-2afd-4630-a777-fbe11de97baa", "username": "PP", "password": "12345", "session_token": None},
+    {"user_id": "8ba44281-fa9f-43e9-b733-a370c0a0f100", "username": "QAA", "password": "25765", "session_token": None},
+]
